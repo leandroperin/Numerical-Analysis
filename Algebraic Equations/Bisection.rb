52d41d26ea3a4bee29c69789940d75959838438b
Leandro Perin <leandro.perino@gmail.com>
2017-05-22 19:55:56 -0300

    Bisection Optimized

--- Algebraic Equations/Bisection.rb
+++ Algebraic Equations/Bisection.rb
@@ -3,26 +3,29 @@ def function(x)
 end
 
 accuracy = 10**-6
-a = 0.to_f
-b = 1.to_f
+a = 0.0
+b = 0.5
 k = 0
 
-x = (a+b) / 2
+while function(a) * function(b) > 0
+	a = b;
+	b += 0.5;
+end
 
-if b-a >= accuracy	
-	M = function(a)
+while (b-a).abs > accuracy
+	x = (a+b) / 2
 	
-	while (b-a) >= accuracy		
-		if M*function(x) > 0
-			a = x
-		else
-			b = x
-		end
-		
-		x = (a+b) / 2
-
-		k += 1
+	if function(x) == 0
+		break
 	end
+	
+	if function(a) * function(x) < 0
+		b = x
+	else
+		a = x
+	end
+	
+	k += 1
 end
 
 puts "X = " + x.to_s
